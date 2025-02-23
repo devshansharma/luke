f274271aa0b782da63531f494b8fab04160b7eda
Shan Sharma <saket0805sharma@gmail.com>
2025-02-23 15:07:26 +0530

    delete functionality for collection and environment

---
+++ cmd/environment/delete.go
@@ -0,0 +1,56 @@
+/*
+Copyright © 2025 Shantanu Sharma sharmashan0805@gmail.com
+*/
+package environment
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/devshansharma/luke/internal/env"
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Args:  cobra.ExactArgs(1),
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		writer := parseGlobalFlags(cmd, args)
+		defer writer.Close()
+
+		id := args[0]
+
+		idNum, err := strconv.Atoi(id)
+		if err != nil {
+			writer.Error(err.Error())
+			os.Exit(1)
+		}
+
+		err = env.DeleteHandler(idNum)
+		if err != nil {
+			writer.Error(err.Error())
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	EnvironmentCmd.AddCommand(deleteCmd)
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
--- internal/env/handler.go
+++ internal/env/handler.go
@@ -4,11 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"sort"
 	"strings"
 
 	"errors"
 
+	"github.com/devshansharma/luke/internal/utils"
 	"github.com/devshansharma/luke/pkg/writer"
 	"github.com/google/uuid"
 )
@@ -59,33 +59,52 @@ func AddHandler(cfg Config) error {
 }
 
 func ListHandler() error {
-	homeDir, err := os.UserHomeDir()
+	entries, err := utils.GetDirEntries(fileSuffix)
 	if err != nil {
 		return err
-	}
-	dirPath := fmt.Sprintf("%s/.luke", homeDir)
-
-	dirEntry, err := os.ReadDir(dirPath)
-	if err != nil {
-		return err
-	}
-
-	sort.Slice(dirEntry, func(i, j int) bool {
-		return dirEntry[i].Name() < dirEntry[j].Name()
-	})
-
-	filteredList := make([]os.DirEntry, 0)
-
-	for _, entry := range dirEntry {
-		if strings.HasSuffix(entry.Name(), fileSuffix) {
-			filteredList = append(filteredList, entry)
-		}
 	}
 
 	writer := writer.GetInstance()
-	for index, entry := range filteredList {
+	for index, entry := range entries {
 		name := strings.TrimSuffix(entry.Name(), fileSuffix)
 		writer.Response(fmt.Sprintf("%02d  %s", index+1, strings.ReplaceAll(name, "_", " ")))
+	}
+
+	return nil
+}
+
+func DeleteHandler(arg int) error {
+	entries, err := utils.GetDirEntries(fileSuffix)
+	if err != nil {
+		return err
+	}
+
+	if arg < 1 || arg > len(entries) {
+		return errors.New("invalid id")
+	}
+
+	name := strings.TrimSuffix(entries[arg-1].Name(), fileSuffix)
+	name = strings.ReplaceAll(name, "_", " ")
+
+	resp, err := utils.AskYesOrNo(fmt.Sprintf("Are you sure, you want to delete %q environment", name))
+	if err != nil {
+		return err
+	}
+
+	if resp {
+		dirPath, err := utils.HomeDir()
+		if err != nil {
+			return err
+		}
+
+		fileName := fmt.Sprintf("%s/%s%s", dirPath, strings.ReplaceAll(name, " ", "_"), fileSuffix)
+
+		err = os.Remove(fileName)
+		if err != nil {
+			return err
+		}
+
+		writer.GetInstance().Response(fmt.Sprintf("%q delete successfully", fileName))
 	}
 
 	return nil
